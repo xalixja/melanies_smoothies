8cbc61cdab9890a091324351ff47f1731c4a9acd
xalixja <alicja.12borek@onet.pl>
2026-07-06 16:41:10 +0200

    Increase max ingredient selections from 5 to 6

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
     my_dataframe,
-    max_selections=5
+    max_selections=6
 )
 
 if ingredients_list:
