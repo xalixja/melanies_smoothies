a11d943d7e114c1f252777e6f7aa6d3689f94ba4
xalixja <alicja.12borek@onet.pl>
2026-07-06 17:29:58 +0200

    Add pandas import to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests  
+import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
