34e473d2dc3d3716c128e13b7868ed44fc0b789f
xalixja <alicja.12borek@onet.pl>
2026-07-06 17:04:43 +0200

    Comment out smoothiefroot nutrition display code
    Comment out the code that fetches and displays smoothie nutrition information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + "!", icon="✅")
 
 # New section to display smoothiefroot nutrition information
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 # st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
